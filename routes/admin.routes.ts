82d0247cb24cc65aec486cd7f3049d7bb6c29980
AlbaraaOthman <ao2000@hw.ac.uk>
2023-03-12 13:43:02 +0000

    Added final routes needed
    could probably have done this in one but i cba

--- routes/admin.routes.ts
+++ routes/admin.routes.ts
@@ -31,6 +31,39 @@ adminRoutes.post('/resolve',  async (req: Request) => {
             },
         },
     })
+    
 });
 
+adminRoutes.post('/getUsers',  async (req: Request, res: Response) => {
+    const usercount = await prisma.users.count({
+    });
+    res.json({ status: true, data: { usercount: usercount }});
+});
+
+adminRoutes.post('/getCom',  async (req: Request, res: Response) => {
+    const comcount = await prisma.comments.count({
+    });
+    res.json({ status: true, data: { comcount: comcount }});
+});
+
+adminRoutes.post('/getImage',  async (req: Request, res: Response) => {
+    const imcount = await prisma.image.count({
+    });
+    res.json({ status: true, data: { imcount: imcount }});
+});
+
+adminRoutes.post('/getResponded',  async (req: Request, res: Response) => {
+    const respcount = await prisma.reported_posts.count({
+        where:{
+            issue_resolved: true
+        }
+    });
+    res.json({ status: true, data: { respcount: respcount }});
+});
+
+adminRoutes.post('/getScrap',  async (req: Request, res: Response) => {
+    const scrapcount = await prisma.scrapbook.count({
+    });
+    res.json({ status: true, data: { scrapcount: scrapcount }});
+});
 export default adminRoutes;
